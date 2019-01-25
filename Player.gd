688522900dbd7c4d28cb217e1cf7c1c26d49ab29
Igor Mosyagin <i.mosyagin@lamoda.ru>
2019-01-26 02:38:28 +0300

    Add godot scene for a player

---
+++ Player.gd
@@ -0,0 +1,40 @@
+extends Area2D
+
+export (int) var speed
+var screensize
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	screensize = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2() # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "right"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y < 0:
+		$AnimatedSprite.animation = "up"
+	elif velocity.y > 0:
+		$AnimatedSprite.animation = "down"
